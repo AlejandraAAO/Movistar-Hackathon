c450bc5dbfcd4f42ea07c031bc5922577e5785b5
Danielapariona <daniela.parionac@gmail.com>
2018-11-07 19:17:28 -0500

    Send email

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from 'src/environments/environment';
+import { ROUTES } from './app.routes'; // routes
 
 import { AppComponent } from './app.component';
+import { SendEmailService } from './components/services/send-email.service';
+import { AdministratorComponent } from './components/administrator/administrator.component';
+import { RouterModule } from '@angular/router';
+import { BackofficeComponent } from './components/administrator/backoffice/backoffice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdministratorComponent,
+    BackofficeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
   ],
-  providers: [],
+  providers: [
+    SendEmailService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
