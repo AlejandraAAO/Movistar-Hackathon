085c2083cf85cc3c02c7ea8810c41686e8ece5bd
RosemarieLopezH <rosemarie61@gmail.com>
2018-11-08 05:36:17 -0500

    create data file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AdministratorComponent } from './components/administrator/administrator.component';
 import { BackofficeComponent } from './components/administrator/backoffice/backoffice.component';
 import { PaymentComponent } from './components/payment/payment.component';
+import { PayComponent } from './components/pay/pay.component';
 
 export const ROUTES: Routes = [
 
@@ -9,4 +10,5 @@ export const ROUTES: Routes = [
   { path: '', component: PaymentComponent},
   { path: 'administrador', component: AdministratorComponent },
   { path: 'backoffice', component: BackofficeComponent },
+  { path: 'pagos', component: PayComponent},
 ];
